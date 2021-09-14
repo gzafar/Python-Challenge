54dd970c4bb248f50b7189baa97bd206b7631c6b
unknown <grazielzafar@gmail.com>
2021-09-14 18:18:53 -0500

    your message like: filling data from csv

---
+++ PyPoll/main.py
@@ -0,0 +1,23 @@
+# Mpdule to create file paths across all operating systems
+import os
+
+# Module for reading CSV files
+import csv
+
+csvpath = os.path.join('..', 'PyPoll', 'Resources', 'election_data.csv')
+
+# Read with CSV module
+with open(csvpath) as csvfile:
+
+    # Specify the delimiter
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    #Read header row
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    #Read each row after header
+    for row in csvreader:
+        print(row)
